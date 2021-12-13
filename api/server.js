0b3d8a38c7bd8fb2ba608e899479519a82c38d55
Ladrillo <gabriel@cabrejas.net>
2021-12-13 19:19:51 +0100

    committing to "lecture"

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
-const session = require('express-s')
+const session = require('express-session')
+const Store
 const helmet = require('helmet')
 
 const authRouter = require('./auth/auth-router')
