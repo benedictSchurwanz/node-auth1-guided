8d50501e6271fb8715a907e0cab192ec0c72dd7a
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:19:34 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,13 +3,27 @@ const express = require('express')
 const router = express.Router()
 
 router.post('/register', async (req, res, next) => {
-
+  try {
+    
+  }  catch (err) {
+    next(err)
+  }
 })
 
 router.post('/login', async (req, res, next) => {
-
+  try {
+    
+  } catch (err) {
+    next(err)
+  }
 })
 
-router.get('/logout')
+router.get('/logout', async (req, res, next) => {
+  try {
+    
+  } catch (err) {
+    next(err)
+  }
+})
 
 module.exports = router
