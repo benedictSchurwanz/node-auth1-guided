a0c98d6b509a98f9f9ebe1aed02aac392491a3ca
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:13:39 +0100

    committing to "lecture"

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,5 +1,5 @@
 const router = require("express").Router()
-const {  }
+const {  } = require('../auth/auth-middleware')
 const Users = require("./users-model.js")
 
 router.get("/", (req, res, next) => {
