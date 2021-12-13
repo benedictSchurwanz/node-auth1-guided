e900f66480dc76e3eeb63db611f29ebc777a0141
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:18:33 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/register', async (req, res, next) => {
-
+  const { username, pas} = req.query
 })
 
 router.get('/login', async (req, res, next) => {
