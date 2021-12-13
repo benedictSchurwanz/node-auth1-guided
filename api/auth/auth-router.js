b7aaafb198a1d33e2a1c543c6dfa0893e13643f2
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:59:52 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -31,8 +31,7 @@ router.post('/login', async (req, res, next) => {
       return next({ message: 'invalid credentials', status: 401 })
     }
     const verifies = bcryptjs.compareSync(password, userFromDb.password)
-    console.log(verifies)
-    res.json('lady gaga')
+    
     // 3- recreate the hash using password from req.body
     // 4- compare this agains the hash in the dabase
     // 5- start a session with the logged-in user
