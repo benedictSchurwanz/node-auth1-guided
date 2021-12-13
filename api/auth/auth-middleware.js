f7cb0b16bdaa052d5f871bd3fb4c134f91cebb95
Ladrillo <gabriel@cabrejas.net>
2021-12-13 19:50:23 +0100

    committing to "lecture"

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,7 @@
 function restrict(req, res, next) {
+  // 1- it checked response for a COOKIE header
+  // 2- pulled session id from it
+  // 3- checked if live session by that id
   if (req.session.user) {
     next()
   } else {
