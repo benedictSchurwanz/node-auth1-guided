8d2feb600d89e72e1efe6c1e0f87c0e6370b7519
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:14:19 +0100

    committing to "lecture"

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,8 +1,8 @@
 const router = require("express").Router()
-const {  } = require('../auth/auth-middleware')
+const { restrict } = require('../auth/auth-middleware')
 const Users = require("./users-model.js")
 
-router.get("/", (req, res, next) => {
+router.get("/", restrict, (req, res, next) => {
   Users.find()
     .then(users => {
       res.status(200).json(users)
