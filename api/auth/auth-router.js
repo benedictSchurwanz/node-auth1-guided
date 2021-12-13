d6ca4968403132f63d180180a5b551693e96689a
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:43:51 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -13,7 +13,9 @@ router.post('/register', async (req, res, next) => {
       username,
       password: bcrypt.hashSync(password, 8), // 2^8 rounds
     }
+    console.log(newUser)
     const created = await User.add(newUser)
+    console.log(created)
     res.status(201).json(created)
   }  catch (err) {
     next(err)
