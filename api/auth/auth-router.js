ecb67e25cc7d86644045dc53e2a894a343eafbc4
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:18:03 +0100

    committing to "lecture"

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -2,8 +2,12 @@ const express = require('express')
 
 const router = express.Router()
 
-router.post('/register', async (req, res, next) => {
-  
+router.get('/register', async (req, res, next) => {
+
+})
+
+router.get('/login', async (req, res, next) => {
+
 })
 
 module.exports = router
