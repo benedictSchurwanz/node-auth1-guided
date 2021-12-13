98516923aac040ce28cc89ce1e7f1ab3fac0379f
Ladrillo <gabriel@cabrejas.net>
2021-12-13 18:14:53 +0100

    committing to "lecture"

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -2,6 +2,8 @@ const router = require("express").Router()
 const { restrict } = require('../auth/auth-middleware')
 const Users = require("./users-model.js")
 
+// it could be wired to the whole router
+// or the whole
 router.get("/", restrict, (req, res, next) => {
   Users.find()
     .then(users => {
