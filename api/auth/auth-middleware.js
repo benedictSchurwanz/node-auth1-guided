6e39813c5cf4045b523d3b9619f1eb0e6188d8d2
Ladrillo <gabriel@cabrejas.net>
2021-12-13 19:46:33 +0100

    committing to "lecture"

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,7 +1,10 @@
 function restrict(req, res, next) {
-  // 150 lines of code
-  console.log('restrict midd is kicking in!')
-  next()
+  if (req.session.user) {
+    next()
+  } else {
+    
+  }
+
 }
 
 module.exports = {
